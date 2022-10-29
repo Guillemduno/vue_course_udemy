fa31810d3d97f4ed41f61d601311676f84dd0d7d
Guillem Duñó <guillemduno@gmail.com>
2022-10-29 07:32:41 +0200

    rebuliding app with vue

--- scripts.js
+++ scripts.js
@@ -1,18 +1,32 @@
 
-const inputElement  = document.getElementById('inputElement');
-const button        = document.querySelector("button");
-const ulElement     = document.querySelector('ul');
+Vue.createApp({
+    data() {
+        return{
+            goals:[],
+            enteredValue: ''
+        };
+    },
+    methods: {
+        addGoal(){
+            this.goals.push(this.enteredValue);
+        }
+    }
+}).mount('#app');
 
-function myAlert()
-{
-   let inputValue       = inputElement.value;
+// Vanilla Javascript
+// const inputElement  = document.getElementById('goal');
+// const button        = document.querySelector("button");
+// const ulElement     = document.querySelector('ul');
 
-   if (inputValue !== "") {
-    const listElement    = document.createElement('li');
-    listElement.textContent = inputValue;
-    ulElement.append(listElement);
-    inputElement.value = "";
-   }
-}
+// function addGoal()
+// {
+//    let inputValue       = inputElement.value;
 
-button.addEventListener('click', myAlert);
+//    if (inputValue !== "") {
+//     const listElement    = document.createElement('li');
+//     listElement.textContent = inputValue;
+//     ulElement.append(listElement);
+//     inputElement.value = "";
+//    }
+// }
+// button.addEventListener('click', addGoal);s
