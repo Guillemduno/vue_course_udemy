5dd2ecf60b454036295f677889e55d3e1a62de6e
Guillem Duñó <guillemduno@gmail.com>
2022-12-13 06:09:07 +0100

    computed and watch

--- basics-05-using-the-native-event-object/basics-05-using-the-native-event-object/app.js
+++ basics-05-using-the-native-event-object/basics-05-using-the-native-event-object/app.js
@@ -2,18 +2,44 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-      name: ''
+      name: '',
+      lastName:'',
+      fullName:''
     };
   },
+  watch:{
+    // name(value){
+    //   if (value === "") {
+    //     this.fullName = ""
+    //   } else {
+    //     this.fullName = value + " "+this.fullName;
+    //   }
+    // },
+    // fullName(value){
+    //   if (value === "") {
+    //     this.fullName = ""
+    //   } else {
+    //     this.fullName = this.name + " "+ value;
+    //   }
+    // }
+  },
+  computed:{
+    fullName(){
+      console.log("Running again");
+      if (this.name === "" || this.lastName === "") {
+        return "";
+      }else{
+        return this.name + " "+this.lastName;
+      }
+    }
+  },
   methods: {
-    setName(event, lastName) {
-      // this.name = event.target.value + ' ' + lastName;
-      this.name = event.target.value;
-    },
     add(num) {
+      console.log("button add pressed!")
       this.counter = this.counter + num;
     },
     reduce(num) {
+      console.log("button reduce pressed!")
       this.counter = this.counter - num;
       // this.counter--;
     },
